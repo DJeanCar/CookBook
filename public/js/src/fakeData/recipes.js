66e56ba81811366ce20917ee362e219db273dfc9
Jean Carlos <jean_carlos104@hotmail.com>
2016-06-14 18:17:10 -0500

    fix bug on my reducer

--- public/js/src/fakeData/recipes.js
+++ public/js/src/fakeData/recipes.js
@@ -1,14 +1,3 @@
-import { List, Map } from "immutable";
-
-/*export const recipes = List([
-  Map({ id: 0, stars: 5, name: "Ceviche", date: "15/12/2016", author: "Jean Mariños", category: "Pescado"}),
-  Map({ id: 1, stars: 4, name: "Arroz con Pollo", date: "15/12/2016", author: "Miguel Villanueva", category: "Criollo"}),
-  Map({ id: 2, stars: 3, name: "Home Made Bolognese Pasta", date: "01/05/2016", author: "Julio Grados", category: "Pastas"}),
-  Map({ id: 3, stars: 4, name: "Arroz con Mariscos", date: "10/07/2016", author: "Edwin Gonzales", category: "pescado"}),
-  Map({ id: 4, stars: 3.4, name: "Ceviche", date: "15/12/2016", author: "Jean Mariños", category: "pescado"}),
-  Map({ id: 5, stars: 4, name: "Arroz con Pollo", date: "15/12/2016", author: "Miguel Villanueva", category: "Criollo"}),
-  Map({ id: 6, stars: 3, name: "Home Made Bolognese Pasta", date: "31/07/2016", author: "Julio Grados", category: "Pastas"}),
-])*/
 export const recipes = [
   { id: 0, stars: 5, name: "Ceviche", date: "15/12/2016", author: "Jean Mariños", category: "Pescado"},
   { id: 1, stars: 4, name: "Arroz con Pollo", date: "15/12/2016", author: "Miguel Villanueva", category: "Criollo"},
